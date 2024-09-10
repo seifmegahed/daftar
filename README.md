608a9373e8b1609c0f798df6c29f73c8af48d20e
seifmegahed <seifmegahed@me.com>
2024-09-10 19:40:04 +0300

    updated start-db script

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 An ARP Web App to manage Engineering Business's contracting projects and resources
 
+This web app is not meant to be deployed to a cloud provider.
+It uses the server's filesystem to store and retrieve files, and might use web sockets in the future.
+So Vercel, Netlify, or any other hosting provider is not recommended.
+
 ## License
 
 MIT
--- start-database.sh
+++ start-database.sh
@@ -9,7 +9,9 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME="daftar-postgres"
+PROJECT_NAME=$(grep '"name"' package.json | head -n 1 | sed -E 's/.*"name": *"([^"]+)".*/\1/')
+
+DB_CONTAINER_NAME="$PROJECT_NAME-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -50,6 +52,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=daftar \ 
+  -e POSTGRES_DB=$PROJECT_NAME \
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
