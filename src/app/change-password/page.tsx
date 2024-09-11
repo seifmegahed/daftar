59668aeac181a7f1a47253f237c5a939552b18ea
seifmegahed <seifmegahed@me.com>
2024-09-11 13:02:52 +0300

    impl pages for login and change password

---
+++ src/app/change-password/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { defaultValues, schema, type ChangePasswordFormType } from "./schema";
+import { changePasswordAction } from "@/server/actions/auth/change-password";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function LoginForm() {
+  const form = useForm<ChangePasswordFormType>({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues,
+  });
+
+  const onSubmit = (data: ChangePasswordFormType) => {
+    changePasswordAction(data)
+      .then(() => console.log("Password changed"))
+      .catch((error) => console.error("Error changing password:", error));
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardHeader>
+              <CardTitle className="text-2xl">Change Password</CardTitle>
+              <CardDescription>
+                Enter your username below to change your password.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2">
+                    <Label htmlFor="username">User Name</Label>
+                    <Input id="username" type="username" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" type="password" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="verifyPassword"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2">
+                    <Label htmlFor="verify-password">Verify Password</Label>
+                    <Input id="verify-password" type="password" {...field} />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full" type="submit">
+                Sign up
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </div>
+  );
+}
