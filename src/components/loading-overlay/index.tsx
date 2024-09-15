4fc14ea97c03b317a1904d9ccd54a1d7b0a05697
seifmegahed <seifmegahed@me.com>
2024-09-15 11:41:47 +0300

    loading overlay to adhere to theme colors

--- src/components/loading-overlay/index.tsx
+++ src/components/loading-overlay/index.tsx
@@ -3,7 +3,7 @@ import Loading from "@/components/loading";
 function LoadingOverlay(props: { state: boolean }) {
   if (!props.state) return null;
   return (
-    <div className="absolute z-50 flex h-full w-full items-center justify-center bg-white/50">
+    <div className="absolute z-50 flex h-full w-full items-center justify-center bg-background/50">
       <Loading />
     </div>
   );
