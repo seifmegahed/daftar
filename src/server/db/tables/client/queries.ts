304bc982e113287b8c31d3dc1560276ae9df3b4f
seifmegahed <seifmegahed@me.com>
2024-09-19 00:34:08 +0300

    getClientFull query

--- src/server/db/tables/client/queries.ts
+++ src/server/db/tables/client/queries.ts
@@ -3,6 +3,8 @@ import { type ClientDataType, clientsTable } from "./schema";
 import type { ReturnTuple } from "@/utils/type-utils";
 import { asc, eq } from "drizzle-orm";
 import { getErrorMessage } from "@/lib/exceptions";
+import { contactsTable } from "../contact/schema";
+import { addressesTable } from "../address/schema";
 
 /**
  * Getters
@@ -28,6 +30,81 @@ export const getAllClientsBrief = async (): Promise<
       .orderBy(asc(clientsTable.id));
 
     return [allClients, null];
+  } catch (error) {
+    return [null, getErrorMessage(error)];
+  }
+};
+
+type ContactDataType = {
+  id: number;
+  name: string;
+  phoneNumber: string | null;
+  email: string | null;
+};
+
+type AddressDataType = {
+  id: number;
+  name: string;
+  addressLine: string;
+  country: string;
+  city: string | null;
+};
+
+type ClientFullType = {
+  client: ClientDataType;
+  contacts: ContactDataType[];
+  addresses: AddressDataType[];
+};
+
+export const getClientFull = async (
+  id: number,
+): Promise<ReturnTuple<ClientFullType>> => {
+  try {
+    const contactsSubQuery = db
+      .select({
+        id: contactsTable.id,
+        name: contactsTable.name,
+        phoneNumber: contactsTable.phoneNumber,
+        email: contactsTable.email,
+      })
+      .from(contactsTable)
+      .where(eq(contactsTable.clientId, id))
+      .as("contacts");
+
+    const addressesSubQuery = db
+      .select({
+        id: addressesTable.id,
+        name: addressesTable.name,
+        addressLine: addressesTable.addressLine,
+        city: addressesTable.city,
+        country: addressesTable.country,
+      })
+      .from(addressesTable)
+      .where(eq(addressesTable.clientId, id))
+      .as("addresses");
+
+    const client = await db
+      .select()
+      .from(clientsTable)
+      .leftJoin(contactsSubQuery, eq(clientsTable.id, contactsTable.clientId))
+      .leftJoin(addressesSubQuery, eq(clientsTable.id, addressesTable.clientId))
+      .where(eq(clientsTable.id, id));
+
+    if (!client.length) throw new Error("Error getting client");
+
+    const reducedClient = client.reduce((accumulator, current) => {
+      return {
+        client: current.client,
+        contacts: current.contacts
+          ? accumulator.contacts.concat(current.contacts)
+          : accumulator.contacts,
+        addresses: current.addresses
+          ? accumulator.addresses.concat(current.addresses)
+          : accumulator.addresses,
+      };
+    }, {} as ClientFullType);
+
+    return [reducedClient, null];
   } catch (error) {
     return [null, getErrorMessage(error)];
   }
