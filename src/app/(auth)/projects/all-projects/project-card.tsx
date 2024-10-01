77035799ddda62e28653a7038211e627db65a18c
seifmegahed <seifmegahed@me.com>
2024-10-01 12:46:27 +0300

    project card context menu

--- src/app/(auth)/projects/all-projects/project-card.tsx
+++ src/app/(auth)/projects/all-projects/project-card.tsx
@@ -1,31 +1,80 @@
+"use client";
+
 import { getStatusLabel } from "@/data/lut";
 import Link from "next/link";
 import { format } from "date-fns";
 import { type BriefProjectType } from "@/server/db/tables/project/queries";
+import { DotsVerticalIcon } from "@radix-ui/react-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
 
 const ProjectCard = ({ project }: { project: BriefProjectType }) => {
   return (
-    <Link href={`/project/${project.id}`}>
-      <div className="flex cursor-pointer items-center gap-5 rounded-xl border p-4 hover:bg-muted">
-        <p className="w-8 text-right text-2xl font-bold text-foreground">
-          {project.id}
-        </p>
-        <div className="flex w-full items-center justify-between">
-          <div>
-            <p className="text-foreground">{project.name}</p>
-            <p className="text-xs text-muted-foreground">
+    <div className="flex items-center gap-5 rounded-xl border p-4">
+      <Link href={`/project/${project.id}`}>
+        <div className="flex cursor-pointer items-center justify-center">
+          <p className="w-10 text-right text-2xl font-bold text-foreground">
+            {project.id}
+          </p>
+        </div>
+      </Link>
+      <div className="flex w-full items-center justify-between">
+        <div>
+          <Link href={`/project/${project.id}`}>
+            <p className="cursor-pointer text-foreground hover:underline">
+              {project.name}
+            </p>
+          </Link>
+          <Link href={`/client/${project.clientId}`}>
+            <p className="cursor-pointer text-xs text-muted-foreground hover:underline">
               {project.clientName}
             </p>
-          </div>
-          <div className="w-36 text-right">
-            <p className="text-foreground">{getStatusLabel(project.status)}</p>
-            <p className="text-xs text-muted-foreground">
-              {format(project.createdAt, "PP")}
-            </p>
-          </div>
+          </Link>
+        </div>
+        <div className="w-36 text-right">
+          <p className="text-foreground">{getStatusLabel(project.status)}</p>
+          <p className="text-xs text-muted-foreground">
+            {format(project.createdAt, "PP")}
+          </p>
         </div>
       </div>
-    </Link>
+      <ProjectCardContextMenu projectId={project.id} />
+    </div>
+  );
+};
+
+const ProjectCardContextMenu = ({ projectId }: { projectId: number }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="z-50 flex cursor-pointer items-center justify-center rounded-full p-2 hover:bg-muted">
+          <DotsVerticalIcon />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <Link href={`/project/${projectId}`}>
+          <DropdownMenuItem>Project Page</DropdownMenuItem>
+        </Link>
+        <Link href={`/project/${projectId}/items`}>
+          <DropdownMenuItem>Project Items</DropdownMenuItem>
+        </Link>
+        <Link href={`/project/${projectId}/documents`}>
+          <DropdownMenuItem>Project Documents</DropdownMenuItem>
+        </Link>
+        <Separator className="my-1" />
+        <Link href={`/project/${projectId}/edit`}>
+          <DropdownMenuItem>Edit Project</DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem>
+          <p className="text-red-500">Delete Project</p>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
--- src/app/(auth)/projects/all-projects/project-list-skeleton.tsx
+++ src/app/(auth)/projects/all-projects/project-list-skeleton.tsx
@@ -12,18 +12,19 @@ function SkeletonList({ pageLimit = 10 }: { pageLimit?: number }) {
 
 const SkeletonCard = () => {
   return (
-    <div className="flex cursor-pointer items-center gap-5 rounded-xl border p-4 hover:bg-muted">
-      <Skeleton className="h-8 w-8" />
+    <div className="flex items-center gap-5 rounded-xl border p-4">
+      <Skeleton className="ml-1 h-8 w-10" />
       <div className="flex w-full items-center justify-between">
         <div className="flex flex-col gap-2">
           <Skeleton className="h-[1.2rem] w-[15rem]" />
           <Skeleton className="h-[0.8rem] w-[12rem]" />
         </div>
-        <div className="w-36 flex flex-col gap-2 items-end">
+        <div className="flex w-36 flex-col items-end gap-2">
           <Skeleton className="h-[1.2rem] w-[10rem]" />
           <Skeleton className="h-[0.8rem] w-[7rem]" />
         </div>
       </div>
+      <Skeleton className="w-8 h-7 rounded-full" />
     </div>
   );
 };
