14cf8630aa7c65843b7a3e814b34b3748f2e0a53
seifmegahed <seifmegahed@me.com>
2024-10-02 14:06:48 +0300

    suppliers documents

--- src/app/(auth)/client/[id]/documents/page.tsx
+++ src/app/(auth)/client/[id]/documents/page.tsx
@@ -6,7 +6,7 @@ async function ClientDocumentsPage({ params }: { params: { id: string } }) {
   if (error !== null) return <p>Error: {error}</p>;
   return (
     <div className="flex flex-col gap-10">
-      <h1 className="text-3xl">Project Documents</h1>
+      <h1 className="text-3xl">Client&apos;s Documents</h1>
       <div>
         <DocumentsList documents={documents} />
       </div>
--- src/app/(auth)/supplier/[id]/documents/page.tsx
+++ src/app/(auth)/supplier/[id]/documents/page.tsx
@@ -1,12 +1,15 @@
+import DocumentsList from "@/components/documents-list";
 import { getSupplierDocumentsAction } from "@/server/actions/documents";
 
 async function SupplierDocumentsPage({ params }: { params: { id: string } }) {
   const [documents, error] = await getSupplierDocumentsAction(Number(params.id));
   if (error !== null) return <p>Error: {error}</p>;
   return (
-    <div>
-      <p>Supplier Documents:</p>
-      <pre>{JSON.stringify(documents, null, 2)}</pre>
+    <div className="flex flex-col gap-10">
+      <h1 className="text-3xl">Supplier&apos;s Documents</h1>
+      <div>
+        <DocumentsList documents={documents} />
+      </div>
     </div>
   );
 }
