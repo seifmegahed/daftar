af17023efb556c68efa9a63f9674f8d04e7fa420
seifmegahed <seifmegahed@me.com>
2024-10-02 14:07:45 +0300

    items documents

--- src/app/(auth)/item/[id]/documents/page.tsx
+++ src/app/(auth)/item/[id]/documents/page.tsx
@@ -1,12 +1,15 @@
+import DocumentsList from "@/components/documents-list";
 import { getItemDocumentsAction } from "@/server/actions/documents";
 
 async function ClientDocumentsPage({ params }: { params: { id: string } }) {
   const [documents, error] = await getItemDocumentsAction(Number(params.id));
   if (error !== null) return <p>Error: {error}</p>;
   return (
-    <div>
-      <p>Item Documents:</p>
-      <pre>{JSON.stringify(documents, null, 2)}</pre>
+    <div className="flex flex-col gap-10">
+      <h1 className="text-3xl">Item&apos;s Documents</h1>
+      <div>
+        <DocumentsList documents={documents} />
+      </div>
     </div>
   );
 }
