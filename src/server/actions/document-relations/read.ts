19d3d2e0d7b88c2b01b6e757c3c80f4af42124a8
seifmegahed <seifmegahed@me.com>
2024-10-04 21:56:30 +0300

    document relations get and count actions and queries

--- src/server/actions/document-relations/read.ts
+++ src/server/actions/document-relations/read.ts
@@ -6,6 +6,21 @@ import {
   getItemDocuments,
   getProjectDocuments,
   getDocumentRelationsCount,
+  documentClientsCount,
+  documentItemsCount,
+  documentProjectsCount,
+  documentSuppliersCount,
+  getDocumentClients,
+  getDocumentItems,
+  getDocumentProjects,
+  getDocumentSuppliers,
+} from "@/server/db/tables/document-relation/queries";
+
+import type {
+  DocumentClientsType,
+  DocumentItemsType,
+  DocumentProjectsType,
+  DocumentSuppliersType,
 } from "@/server/db/tables/document-relation/queries";
 
 import type { SimpDoc } from "@/server/db/tables/document/queries";
@@ -79,6 +94,70 @@ export const getDocumentRelationsCountAction = async (
   id: number,
 ): Promise<ReturnTuple<number>> => {
   const [count, countError] = await getDocumentRelationsCount(id);
+  if (countError !== null) return [null, countError];
+  return [count, null];
+};
+
+export const getDocumentClientsAction = async (
+  id: number,
+): Promise<ReturnTuple<DocumentClientsType[]>> => {
+  const [clients, clientsError] = await getDocumentClients(id);
+  if (clientsError !== null) return [null, clientsError];
+  return [clients, null];
+};
+
+export const getDocumentClientsCountAction = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  const [count, countError] = await documentClientsCount(id);
+  if (countError !== null) return [null, countError];
+  return [count, null];
+};
+
+export const getDocumentItemsAction = async (
+  id: number,
+): Promise<ReturnTuple<DocumentItemsType[]>> => {
+  const [items, itemsError] = await getDocumentItems(id);
+  if (itemsError !== null) return [null, itemsError];
+  return [items, null];
+};
+
+export const getDocumentItemsCountAction = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  const [count, countError] = await documentItemsCount(id);
+  if (countError !== null) return [null, countError];
+  return [count, null];
+};
+
+export const getDocumentProjectsAction = async (
+  id: number,
+): Promise<ReturnTuple<DocumentProjectsType[]>> => {
+  const [projects, projectsError] = await getDocumentProjects(id);
+  if (projectsError !== null) return [null, projectsError];
+  return [projects, null];
+};
+
+export const getDocumentProjectsCountAction = async (
+  id: number,
+) => {
+  const [count, countError] = await documentProjectsCount(id);
+  if (countError !== null) return [null, countError];
+  return [count, null];
+};
+
+export const getDocumentSuppliersAction = async (
+  id: number,
+): Promise<ReturnTuple<DocumentSuppliersType[]>> => {
+  const [suppliers, suppliersError] = await getDocumentSuppliers(id);
+  if (suppliersError !== null) return [null, suppliersError];
+  return [suppliers, null];
+};
+
+export const getDocumentSuppliersCountAction = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  const [count, countError] = await documentSuppliersCount(id);
   if (countError !== null) return [null, countError];
   return [count, null];
 };
--- src/server/db/tables/document-relation/queries.ts
+++ src/server/db/tables/document-relation/queries.ts
@@ -1,13 +1,20 @@
-import { count, eq } from "drizzle-orm";
+import { count, eq, and, desc, isNotNull } from "drizzle-orm";
 import { db } from "@/server/db";
 
 import { documentRelationsTable } from "./schema";
-import { documentsTable } from "@/server/db/schema";
+import {
+  clientsTable,
+  documentsTable,
+  itemsTable,
+  projectsTable,
+  suppliersTable,
+} from "@/server/db/schema";
 
 import type { DocumentRelationsType } from "./schema";
 import type { DocumentDataType } from "@/server/db/tables/document/schema";
 import type { SimpDoc } from "@/server/db/tables/document/queries";
 import type { ReturnTuple } from "@/utils/type-utils";
+import { z } from "zod";
 
 export const insertDocumentWithRelation = async (
   document: DocumentDataType,
@@ -158,9 +165,9 @@ export const deleteDocumentRelation = async (
   }
 };
 
-export const getDocumentRelationsCount = async (id: number): Promise<
-  ReturnTuple<number>
-> => {
+export const getDocumentRelationsCount = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
   try {
     const [result] = await db
       .select({ count: count() })
@@ -172,5 +179,280 @@ export const getDocumentRelationsCount = async (id: number): Promise<
   } catch (error) {
     console.log(error);
     return [null, "Error getting document relations count"];
+  }
+};
+
+const documentProjectsSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  clientId: z.number(),
+  clientName: z.string(),
+  status: z.number(),
+  createdAt: z.date(),
+});
+
+export type DocumentProjectsType = z.infer<typeof documentProjectsSchema>;
+
+export const getDocumentProjects = async (
+  id: number,
+): Promise<ReturnTuple<DocumentProjectsType[]>> => {
+  try {
+    const projects = await db
+      .select({
+        id: projectsTable.id,
+        name: projectsTable.name,
+        clientId: projectsTable.clientId,
+        clientName: clientsTable.name,
+        status: projectsTable.status,
+        createdAt: projectsTable.createdAt,
+      })
+      .from(documentRelationsTable)
+      .leftJoin(
+        projectsTable,
+        eq(documentRelationsTable.projectId, projectsTable.id),
+      )
+      .leftJoin(clientsTable, eq(projectsTable.clientId, clientsTable.id))
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(projectsTable.id),
+        ),
+      )
+      .orderBy(desc(projectsTable.createdAt));
+
+    if (!projects) return [null, "Error getting document projects"];
+    const parsedProjects = z.array(documentProjectsSchema).safeParse(projects);
+
+    if (parsedProjects.error) return [null, "Error getting document projects"];
+
+    return [parsedProjects.data, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document projects"];
+  }
+};
+
+export const documentProjectsCount = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  try {
+    const [projects] = await db
+      .select({ count: count() })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.projectId),
+        ),
+      )
+      .limit(1);
+
+    if (!projects) return [null, "Error getting document projects count"];
+    return [projects.count, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document projects count"];
+  }
+};
+
+const documentClientsSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  registrationNumber: z.string().nullable(),
+  createdAt: z.date(),
+});
+
+export type DocumentClientsType = z.infer<typeof documentClientsSchema>;
+
+export const getDocumentClients = async (
+  id: number,
+): Promise<ReturnTuple<DocumentClientsType[]>> => {
+  try {
+    const clients = await db
+      .select({
+        id: clientsTable.id,
+        name: clientsTable.name,
+        registrationNumber: clientsTable.registrationNumber,
+        createdAt: clientsTable.createdAt,
+      })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.projectId),
+        ),
+      )
+      .leftJoin(clientsTable, eq(documentRelationsTable.clientId, clientsTable.id))
+      .orderBy(desc(clientsTable.createdAt));
+
+    const parsedClients = z.array(documentClientsSchema).safeParse(clients);
+
+    if (parsedClients.error) return [null, "Error getting document clients"];
+
+    return [parsedClients.data, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document clients"];
+  }
+};
+
+export const documentClientsCount = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  try {
+    const [clients] = await db
+      .select({ count: count() })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.projectId),
+        ),
+      )
+      .limit(1);
+
+    if (!clients) return [null, "Error getting document clients count"];
+    return [clients.count, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document clients count"];
+  }
+};
+
+const documentSuppliersSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  field: z.string(),
+  registrationNumber: z.string().nullable(),
+  createdAt: z.date(),
+});
+
+export type DocumentSuppliersType = z.infer<typeof documentSuppliersSchema>;
+
+export const getDocumentSuppliers = async (
+  id: number,
+): Promise<ReturnTuple<DocumentSuppliersType[]>> => {
+  try {
+    const suppliers = await db
+      .select({
+        id: suppliersTable.id,
+        name: suppliersTable.name,
+        field: suppliersTable.field,
+        registrationNumber: suppliersTable.registrationNumber,
+        createdAt: suppliersTable.createdAt,
+      })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.supplierId),
+        ),
+      )
+      .leftJoin(
+        suppliersTable,
+        eq(documentRelationsTable.supplierId, suppliersTable.id),
+      )
+      .orderBy(desc(suppliersTable.createdAt));
+
+    const parsedSuppliers = z
+      .array(documentSuppliersSchema)
+      .safeParse(suppliers);
+
+    if (parsedSuppliers.error)
+      return [null, "Error getting document suppliers"];
+
+    return [parsedSuppliers.data, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document suppliers"];
+  }
+};
+
+export const documentSuppliersCount = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  try {
+    const [suppliers] = await db
+      .select({ count: count() })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.supplierId),
+        ),
+      )
+      .limit(1);
+
+    if (!suppliers) return [null, "Error getting document suppliers count"];
+    return [suppliers.count, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document suppliers count"];
+  }
+};
+
+const documentItemsSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  make: z.string(),
+  mpn: z.string(),
+  createdAt: z.date(),
+});
+
+export type DocumentItemsType = z.infer<typeof documentItemsSchema>;
+
+export const getDocumentItems = async (
+  id: number,
+): Promise<ReturnTuple<DocumentItemsType[]>> => {
+  try {
+    const items = await db
+      .select({
+        id: itemsTable.id,
+        name: itemsTable.name,
+        make: itemsTable.make,
+        mpn: itemsTable.mpn,
+        createdAt: itemsTable.createdAt,
+      })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.itemId),
+        ),
+      )
+      .leftJoin(itemsTable, eq(documentRelationsTable.itemId, itemsTable.id))
+      .orderBy(desc(itemsTable.createdAt));
+
+    const parsedItems = z.array(documentItemsSchema).safeParse(items);
+
+    if (parsedItems.error) return [null, "Error getting document items"];
+
+    return [parsedItems.data, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document items"];
+  }
+};
+
+export const documentItemsCount = async (
+  id: number,
+): Promise<ReturnTuple<number>> => {
+  try {
+    const [items] = await db
+      .select({ count: count() })
+      .from(documentRelationsTable)
+      .where(
+        and(
+          eq(documentRelationsTable.documentId, id),
+          isNotNull(documentRelationsTable.itemId),
+        ),
+      )
+      .limit(1);
+
+    if (!items) return [null, "Error getting document items count"];
+    return [items.count, null];
+  } catch (error) {
+    console.log(error);
+    return [null, "Error getting document items count"];
   }
 };
