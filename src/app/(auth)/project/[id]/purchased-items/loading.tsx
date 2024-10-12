650a811b60becdb97385d45337b645ac9404aad6
seifmegahed <seifmegahed@me.com>
2024-10-12 19:29:36 +0300

    some more alt lists loading pages

---
+++ src/app/(auth)/project/[id]/purchased-items/loading.tsx
@@ -0,0 +1,7 @@
+import { CardBAltListSkeleton } from "@/components/skeletons/alt-list-skeletons";
+
+function CardBAltListLoadingPage() {
+  return <CardBAltListSkeleton />;
+}
+
+export default CardBAltListLoadingPage;
