3d9229d4a08ecf9d28967136b58cbc031b52f3a0
seifmegahed <seifmegahed@me.com>
2024-10-14 23:20:50 +0300

    type documentation

--- src/utils/type-utils/index.ts
+++ src/utils/type-utils/index.ts
@@ -1,9 +1,47 @@
-export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
-  U[keyof U];
-
-export type ReturnTuple<T> = readonly [T, null] | readonly [null, string];
-
+/**
+ * Type utility to make a certain property of an object optional
+ * 
+ * @example
+ * ```tsx
+ * type User = {
+ *   id: number;
+ *   name: string;
+ *   email: string;
+ * }
+ * 
+ * type UserWithOptionalEmail = PartialBy<User, 'email'>
+ * 
+ * const user: UserWithOptionalEmail = {
+ *   id: 1,
+ *   name: 'John Doe',
+ * }
+ * ```
+ * 
+ * @param T The type to make a property optional
+ * @param K The property to make optional
+ */
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+/**
+ * GO-like return type to ensure proper error handling
+ *
+ * This type ensures the the return is either the generic value or the error message.
+ * 
+ * Acts as an XOR gate (Either Or) (Never Neither) (Never Both)
+ * 
+ * This forces the caller to handle the error case.
+ *
+ * @example
+ * ```tsx
+ * const [value, error] = await getUserByIdAction(id);
+ * if (error !== null) return <p>Error: {error}</p>;
+ * return <p>User: {value.name}</p>;
+ * ```
+ *
+ * @param T The generic type to return
+ * @returns A tuple of the generic type and a null or a null and a string error message
+ */
+export type ReturnTuple<T> = readonly [T, null] | readonly [null, string];
 
 export type SearchParamsPropsType = {
   page?: string;
