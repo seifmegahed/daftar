efbf2ebf9a4a0f7b4d207dff241bf5b9795efcab
seifmegahed <seifmegahed@me.com>
2024-10-15 13:12:26 +0300

    top nav mobile styling fixes

--- src/components/nav/top-nav/index.tsx
+++ src/components/nav/top-nav/index.tsx
@@ -15,7 +15,7 @@ async function TopNav() {
   }
 
   return (
-    <header className="sticky top-0 z-20 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky top-0 z-20 flex h-16 items-center gap-4 border-b bg-background px-3 md:px-6">
       <DesktopNav admin={currentUser.role === "admin"} />
       <MobileNav admin={currentUser.role === "admin"} />
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
--- src/components/nav/top-nav/mobile-nav/index.tsx
+++ src/components/nav/top-nav/mobile-nav/index.tsx
@@ -2,34 +2,33 @@ import Link from "next/link";
 import { Menu } from "lucide-react";
 import { BookmarkIcon } from "@/icons";
 
-import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import NavLinks from "../nav-links";
 import { Description, DialogTitle } from "@radix-ui/react-dialog";
 
-function MobileNav({
-  admin,
-}: {
-  admin: boolean;
-}) {
+function MobileNav({ admin }: { admin: boolean }) {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+        <div className="cursor-pointer md:hidden">
           <Menu className="h-5 w-5" />
           <DialogTitle className="hidden">Open menu</DialogTitle>
           <Description className="sr-only">Open menu</Description>
-        </Button>
+        </div>
       </SheetTrigger>
       <SheetContent side="left">
         <nav className="grid gap-6 text-lg font-medium">
           <Link
-            href="#"
+            href="/"
             className="flex items-center gap-2 text-lg font-semibold"
           >
             <BookmarkIcon />
           </Link>
-          <NavLinks admin={admin} />
+          <NavLinks admin={admin} />ƒ
         </nav>
       </SheetContent>
     </Sheet>
