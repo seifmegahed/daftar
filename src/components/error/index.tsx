1aa8deba6ab5c26f6a506c045f650d367464a173
seifmegahed <seifmegahed@me.com>
2024-10-16 21:29:14 +0300

    error image svg

--- src/components/error/index.tsx
+++ src/components/error/index.tsx
@@ -1,11 +1,5 @@
-"use client";
-
-import { useTheme } from "next-themes";
-import Image from "next/image";
+import PokeBoi from "@/icons/poke-boi";
 import Balancer from "react-wrap-balancer";
-
-const darkUrl = "/poke-dark.png";
-const lightUrl = "/poke-light.png";
 
 function ErrorPage({
   title = "Oops! Something went wrong",
@@ -14,22 +8,15 @@ function ErrorPage({
   title?: string;
   message?: string;
 }) {
-  const { theme } = useTheme();
   return (
     <div className="flex h-full w-full flex-col items-center gap-4 text-center text-muted-foreground">
-      <Image
-        src={theme === "dark" ? darkUrl : lightUrl}
-        alt="Poking the dead"
-        width={150}
-        height={150}
-        loading="lazy"
-      />
-      <Balancer>
-        <h1 className="text-2xl font-bold">{title}</h1>
-      </Balancer>
-      <Balancer>
-        <h2>{message}</h2>
-      </Balancer>
+      <PokeBoi />
+      <h1 className="text-2xl font-bold">
+        <Balancer>{title}</Balancer>
+      </h1>
+      <p>
+        <Balancer>{message}</Balancer>
+      </p>
     </div>
   );
 }
--- src/components/nav/top-nav/desktop-nav/index.tsx
+++ src/components/nav/top-nav/desktop-nav/index.tsx
@@ -5,15 +5,16 @@ import NavLinks from "../nav-links";
 
 function DesktopNav({ admin }: { admin: boolean }) {
   return (
-    <nav className="hidden flex-col gap-5 text-lg font-medium md:flex md:flex-row md:items-center md:text-sm">
+    <nav className="hidden flex-col gap-4 text-lg font-medium md:flex md:flex-row md:items-center md:text-sm">
       <Link
         href="#"
         className="flex items-center gap-2 text-lg font-semibold md:text-base"
       >
         <BookmarkIcon />
-        <span className="Dialog-title sr-only">Accounting UI</span>
       </Link>
-      <NavLinks admin={admin} />
+      <div className="flex items-center gap-8">
+        <NavLinks admin={admin} />
+      </div>
     </nav>
   );
 }
