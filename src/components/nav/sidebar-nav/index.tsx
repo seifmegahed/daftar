c5a760897f8965fa474c8d1390d760a43146bc58
seifmegahed <seifmegahed@me.com>
2024-10-20 14:14:04 +0300

    sidenav scroll bar on less than lg only

--- src/components/nav/sidebar-nav/index.tsx
+++ src/components/nav/sidebar-nav/index.tsx
@@ -17,28 +17,32 @@ function SidebarNav({ links }: { links: NavLink[] }) {
   const pathname = usePathname();
 
   return (
-    <nav className="max-w-screen flex h-16 space-x-2 overflow-x-scroll md:px-4 lg:h-full lg:flex-col lg:space-x-0 lg:space-y-1">
-      {links.map((link) => link.hidden ? null : (
-        <Link
-          key={link.href}
-          href={link.href}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            pathname === link.href
-              ? "bg-muted hover:bg-muted"
-              : "group hover:bg-transparent",
-          )}
-        >
-          {link.amount !== undefined ? (
-            <div className="flex w-full items-center justify-between">
-              <p className="group-hover:underline">{link.title}</p>
-              <p className="hidden text-xs font-thin lg:block">{link.amount}</p>
-            </div>
-          ) : (
-            <p className="w-full group-hover:underline">{link.title}</p>
-          )}
-        </Link>
-      ))}
+    <nav className="max-w-screen lg:overflow-x-none flex h-16 space-x-2 overflow-x-scroll md:px-4 lg:h-full lg:flex-col lg:space-x-0 lg:space-y-1">
+      {links.map((link) =>
+        link.hidden ? null : (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              pathname === link.href
+                ? "bg-muted hover:bg-muted"
+                : "group hover:bg-transparent",
+            )}
+          >
+            {link.amount !== undefined ? (
+              <div className="flex w-full items-center justify-between">
+                <p className="group-hover:underline">{link.title}</p>
+                <p className="hidden text-xs font-thin lg:block">
+                  {link.amount}
+                </p>
+              </div>
+            ) : (
+              <p className="w-full group-hover:underline">{link.title}</p>
+            )}
+          </Link>
+        ),
+      )}
     </nav>
   );
 }
