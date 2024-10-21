658c7ba14722500f62ee435de5ff07ba035861f1
seifmegahed <seifmegahed@me.com>
2024-10-21 15:35:53 +0300

    auth between pages loading

---
+++ src/app/(auth)/loading.tsx
@@ -0,0 +1,32 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "@/components/ui/separator";
+import InfoPageSkeleton from "@/components/skeletons/info-page-skeleton";
+
+function BaseLoading() {
+  return (
+    <div className="h-fit min-h-[calc(100vh_-_theme(spacing.16))]">
+      <div className="h-fit min-h-[calc(100vh_-_theme(spacing.16))]">
+        <div className="space-y-6 p-0 pb-16 md:px-6">
+          <div className="space-y-1.5 p-3 md:p-0">
+            <Skeleton className="h-7 w-24" />
+            <Skeleton className="h-6 w-40" />
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col space-y-8 lg:flex-row lg:gap-x-12 lg:space-y-0">
+            <div className="w-full max-w-full lg:-ms-4 lg:w-1/5">
+              <div className="max-w-screen flex h-16 min-w-56 space-x-2 overflow-x-scroll lg:h-full lg:flex-col lg:space-x-0 lg:space-y-1 lg:overflow-x-visible lg:px-4">
+                <Skeleton className="h-8 w-full" />
+                <Skeleton className="h-8 w-full" />
+              </div>
+            </div>
+            <div className="flex-1 lg:max-w-2xl">
+              <InfoPageSkeleton />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BaseLoading;
--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -3,9 +3,12 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
+const test = false;
+
 export default function Page() {
   const navigate = useRouter();
   useEffect(() => {
+    if (test) return;
     navigate.push("/projects");
   });
   return <></>;
