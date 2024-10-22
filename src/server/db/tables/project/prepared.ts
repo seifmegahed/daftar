8589c85ea08146cd2203e2f5ce3a2e500a5b4e9c
seifmegahed <seifmegahed@me.com>
2024-10-22 10:19:55 +0300

    alt query for projects

--- src/server/db/tables/project/prepared.ts
+++ src/server/db/tables/project/prepared.ts
@@ -1,6 +1,12 @@
 import { db } from "@/server/db";
 import { eq, sql } from "drizzle-orm";
-import { clientsTable, projectsTable } from "@/server/db/schema";
+import {
+  addressesTable,
+  clientsTable,
+  contactsTable,
+  projectsTable,
+  usersTable,
+} from "@/server/db/schema";
 
 export const clientProjectsQuery = db
   .select({
@@ -15,6 +21,56 @@ export const clientProjectsQuery = db
   .leftJoin(clientsTable, eq(projectsTable.clientId, clientsTable.id))
   .where(eq(projectsTable.clientId, sql.placeholder("id")))
   .prepare("client_projects");
+
+/**
+ * Same as the one below but in sql format
+ *
+ * Maybe use this one in the future?
+ *
+ * The difference is that this one does not store
+ * client address and contact inside the client object
+ *
+ * so it needs some type refactoring and front end refactoring
+ */
+export const projectByIdQuery2 = db
+  .select({
+    id: projectsTable.id,
+    name: projectsTable.name,
+    status: projectsTable.status,
+    createdAt: projectsTable.createdAt,
+    owner: {
+      id: projectsTable.id,
+      name: usersTable.name,
+    },
+    client: {
+      clientId: projectsTable.clientId,
+      name: clientsTable.name,
+      registrationNumber: clientsTable.registrationNumber,
+      website: clientsTable.website,
+    },
+    primaryAddress: {
+      id: clientsTable.primaryAddressId,
+      addressLine: addressesTable.addressLine,
+      city: addressesTable.city,
+      country: addressesTable.country,
+    },
+    primaryContact: {
+      id: clientsTable.primaryContactId,
+      name: contactsTable.name,
+      email: contactsTable.email,
+      phoneNumber: contactsTable.phoneNumber,
+    },
+  })
+  .from(projectsTable)
+  .where(eq(projectsTable.id, sql.placeholder("id")))
+  .leftJoin(clientsTable, eq(projectsTable.clientId, clientsTable.id))
+  .leftJoin(usersTable, eq(projectsTable.ownerId, usersTable.id))
+  .leftJoin(
+    addressesTable,
+    eq(clientsTable.primaryAddressId, addressesTable.id),
+  )
+  .leftJoin(contactsTable, eq(clientsTable.primaryContactId, contactsTable.id))
+  .prepare("project_by_id2");
 
 export const projectByIdQuery = db.query.projectsTable
   .findFirst({
