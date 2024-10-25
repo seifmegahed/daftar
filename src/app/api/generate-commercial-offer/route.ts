e45c5e2f9020ac767e998f4c99ad4c37506e22ad
seifmegahed <seifmegahed@me.com>
2024-10-25 21:53:32 +0300

    offer template with dynamic table

--- src/app/api/generate-commercial-offer/route.ts
+++ src/app/api/generate-commercial-offer/route.ts
@@ -1,7 +1,10 @@
 import type { NextRequest } from "next/server";
 import { generate } from "@pdfme/generator";
 import { template } from "./template";
+import { text, line, multiVariableText, table, svg } from "@pdfme/schemas";
 import { getProjectByIdAction } from "@/server/actions/projects/read";
+import { getProjectSaleItemsAction } from "@/server/actions/sale-items/read";
+import { numberWithCommas } from "@/utils/common";
 
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
@@ -11,11 +14,32 @@ export async function POST(request: NextRequest) {
   const [project, error] = await getProjectByIdAction(id);
   if (error !== null) return new Response("Not found", { status: 404 });
 
-  const { name, description, startDate } = project;
+  const [saleItems, saleItemsError] = await getProjectSaleItemsAction(id);
+  if (saleItemsError !== null)
+    return new Response("Error getting sale items", { status: 500 });
+
+  const { client } = project;
   const pdf = await generate({
     template,
-    inputs: [{ a: name, b: description, c: startDate }],
-  });
+    inputs: [
+      {
+        billedToInput: client.name,
+        orders: saleItems.map((saleItem) => {
+          const { quantity, name, price } = saleItem;
+          const total = parseFloat(price) * saleItem.quantity;
+          return [
+            name,
+            String(quantity),
+            numberWithCommas(parseFloat(price)),
+            numberWithCommas(total),
+          ];
+        }),
+      },
+    ],
+    plugins: { text, table, line, multiVariableText, svg },
+  }).catch((error) => console.error(error));
+
+  if (!pdf) return new Response("Error generating PDF", { status: 500 });
 
   const blob = new Blob([pdf.buffer], { type: "application/pdf" });
   const blobStream = blob.stream();
