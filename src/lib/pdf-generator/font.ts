85edcaf0cc2e765b8372893b2b3add7557251428
seifmegahed <seifmegahed@me.com>
2024-10-29 17:10:52 +0300

    resolve font path

--- src/lib/pdf-generator/font.ts
+++ src/lib/pdf-generator/font.ts
@@ -1,11 +1,12 @@
 import type { Font } from "@pdfme/common";
 import fs from "node:fs/promises";
+import path from "node:path";
 
 const readexPro = await fs.readFile(
-  "src/fonts/Readex_Pro/static/ReadexPro-ExtraLight.ttf",
+  path.resolve("src/fonts/Readex_Pro/static/ReadexPro-ExtraLight.ttf"),
 );
 const readexProBold = await fs.readFile(
-  "src/fonts/Readex_Pro/static/ReadexPro-SemiBold.ttf",
+  path.resolve("src/fonts/Readex_Pro/static/ReadexPro-SemiBold.ttf"),
 );
 const readexProFontData = Buffer.from(readexPro).toString("base64");
 const readexProBoldFontData = Buffer.from(readexProBold).toString("base64");
