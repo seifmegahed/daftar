a9129a5d635926ce2152bdd21cf6fdb9c4f8fb13
seifmegahed <seifmegahed@me.com>
2024-11-01 18:43:20 +0200

    all projects error messages

--- src/app/[locale]/(auth)/projects/all-projects/projects-list.tsx
+++ src/app/[locale]/(auth)/projects/all-projects/projects-list.tsx
@@ -2,6 +2,7 @@ import { getProjectsBriefAction } from "@/server/actions/projects/read";
 import ProjectCard from "./project-card";
 import ErrorPage from "@/components/error";
 import type { FilterArgs } from "@/components/filter-and-search";
+import { getTranslations } from "next-intl/server";
 
 async function ProjectsList({
   page = 1,
@@ -13,12 +14,14 @@ async function ProjectsList({
   filter?: FilterArgs;
 }) {
   const [projects, error] = await getProjectsBriefAction(page, filter, query);
+  const t = await getTranslations("projects.all-projects-page");
+
   if (error !== null) return <ErrorPage message={error} />;
   if (!projects.length)
     return (
       <ErrorPage
-        title="There seems to be no projects!"
-        message="Start adding projects to see them here."
+        title={t("no-projects-found-error-title")}
+        message={t("no-projects-found-error-message")}
       />
     );
 
