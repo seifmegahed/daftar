68f8ad268ccce91b5ca635380720574b77a2f4d9
seifmegahed <seifmegahed@me.com>
2024-11-01 16:02:12 +0200

    default error page translate

--- src/components/error/index.tsx
+++ src/components/error/index.tsx
@@ -1,18 +1,20 @@
 import PokeBoi from "@/icons/poke-boi";
+import { getTranslations } from "next-intl/server";
 import Balancer from "react-wrap-balancer";
 
-function ErrorPage({
-  title = "Oops! Something went wrong",
+async function ErrorPage({
+  title,
   message,
 }: {
   title?: string;
   message?: string;
 }) {
+  const t = await getTranslations("error");
   return (
     <div className="flex h-full w-full flex-col items-center gap-4 text-center text-muted-foreground">
       <PokeBoi />
       <h1 className="text-2xl font-bold">
-        <Balancer>{title}</Balancer>
+        <Balancer>{title ?? t("title")}</Balancer>
       </h1>
       <p>
         <Balancer>{message}</Balancer>
