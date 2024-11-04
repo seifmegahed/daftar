a374deaad5b77df07ec44e0050986044b77aa904
seifmegahed <seifmegahed@me.com>
2024-11-04 12:41:08 +0200

    client documents page translation

--- src/app/[locale]/(auth)/client/[id]/documents/page.tsx
+++ src/app/[locale]/(auth)/client/[id]/documents/page.tsx
@@ -2,22 +2,27 @@ import { getClientDocumentsAction } from "@/server/actions/document-relations/re
 import DocumentsList from "@/components/documents-list";
 import ListPageWrapper from "@/components/list-page-wrapper";
 import ErrorPage from "@/components/error";
+import { setLocale } from "@/i18n/set-locale";
+import { getTranslations } from "next-intl/server";
 
-async function ClientDocumentsPage({ params }: { params: { id: string } }) {
+async function ClientDocumentsPage({ params }: { params: { id: string; locale: Locale } }) {
+  setLocale(params.locale);
+  const t = await getTranslations("client.documents");
+
   const clientId = parseInt(params.id);
-  if (isNaN(clientId)) return <ErrorPage message="Invalid Client ID" />;
+  if (isNaN(clientId)) return <ErrorPage message={t("invalid-id")} />;
 
   const [documents, error] = await getClientDocumentsAction(clientId);
   if (error !== null) return <ErrorPage message={error} />;
   if (!documents.length)
     return (
-      <ErrorPage title="There seems to be no documents linked to this client yet" />
+      <ErrorPage title={t("no-documents-found-error-message")} />
     );
 
   return (
     <ListPageWrapper
-      title="Client's Documents"
-      subtitle="This is a list of the documents linked to the client."
+      title={t("title")}
+      subtitle={t("subtitle")}
     >
       <DocumentsList documents={documents} />
     </ListPageWrapper>
