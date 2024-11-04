5e31edb4e5dad104ee473efeb3f9c07e564da6be
seifmegahed <seifmegahed@me.com>
2024-11-04 12:38:27 +0200

    client projects page translate

--- src/app/[locale]/(auth)/client/[id]/projects/page.tsx
+++ src/app/[locale]/(auth)/client/[id]/projects/page.tsx
@@ -2,23 +2,27 @@ import ProjectCard from "@/app/[locale]/(auth)/projects/all-projects/project-car
 import ErrorPage from "@/components/error";
 import ListPageWrapper from "@/components/list-page-wrapper";
 import { getClientProjectsAction } from "@/server/actions/projects/read";
+import { setLocale } from "@/i18n/set-locale";
+import { getTranslations } from "next-intl/server";
 
-async function ClientProjectsPage({ params }: { params: { id: string } }) {
+async function ClientProjectsPage({
+  params,
+}: {
+  params: { id: string; locale: Locale };
+}) {
+  setLocale(params.locale);
+  const t = await getTranslations("client.projects");
+
   const clientId = parseInt(params.id);
-  if (isNaN(clientId)) return <ErrorPage message="Invalid client ID" />;
+  if (isNaN(clientId)) return <ErrorPage message={t("invalid-id")} />;
 
   const [projects, error] = await getClientProjectsAction(clientId);
   if (error !== null) return <ErrorPage message={error} />;
   if (!projects.length)
-    return (
-      <ErrorPage title="There seems to be no projects linked to this client yet" />
-    );
+    return <ErrorPage title={t("no-projects-found-error-message")} />;
 
   return (
-    <ListPageWrapper
-      title="Client's Projects"
-      subtitle="This is a list of the client's projects."
-    >
+    <ListPageWrapper title={t("title")} subtitle={t("subtitle")}>
       {projects.map((project) => (
         <ProjectCard key={project.id} project={project} />
       ))}
